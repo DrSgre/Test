fe6e44808d9c09278510779084dcf99eddb80a11
IlDottore <alex.thedoctor200@gmail.com>
2016-01-22 13:37:08 +0100

    La mia app Django, primo commit

---
+++ Test/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Tabella
+
+admin.site.register(Tabella)
+
+
+# Register your models here.
---
+++ Test/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+
+class Tabella(models.Model):
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
